4ab54ebdacede4427633c3dc2eac20883d776ab0
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-10-31 20:54:18 +0700

    Menambah pembuat Upper Hessenberg Matrix

--- src/lib/householder/transform.py
+++ src/lib/householder/transform.py
@@ -3,13 +3,16 @@ import numpy as np
 def householder_matrix(A):
   """Menghitung matriks transformasi Householder."""
 
-  x = np.transpose(A)[0]
+  x = A[:,0]
 
   e = np.identity(len(x))[0] * np.sqrt(x @ x)
   b = x - e
   n = b / np.sqrt(b @ np.transpose(b))
+
+  return hmatrix_calc(len(x), n)
+
+def hmatrix_calc(l,v):
+  """Menghitung matrix transformasi householder matriks A ke matriks yang searah dengan E"""
   
-  nd2 = n[np.newaxis,:]
-
-  return np.identity(len(x)) - 2 * (np.transpose(nd2) @ nd2)
-
+  nd2 = v[np.newaxis,:]
+  return np.identity(l) - 2 * (np.transpose(nd2) @ nd2)
