89d9a30b6eb41b668521f3d4d8271c5cd40e2978
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-11-03 08:58:07 +0700

    Menambahkan Pengetesan Q

--- src/lib/qr/test_qr.py
+++ src/lib/qr/test_qr.py
@@ -6,11 +6,18 @@ def test_qr_1():
 
   q,r = qr_factorization(A)
 
+  # Cek sifat Q
+  IQ = q @ q.transpose()
+  assert np.allclose(np.identity(len(IQ)), IQ)
+
   assert np.allclose(A, q @ r)
 
 def test_qr_2():
   A = np.random.randint(0,255,size=(30,50))
 
   q,r = qr_factorization(A)
+
+  IQ = q @ q.transpose()
+  assert np.allclose(np.identity(len(IQ)), IQ)
 
   assert np.allclose(A, q @ r)
