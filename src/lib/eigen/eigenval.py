ca1707514b769f7e00923a83e60a9676861e7820
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-11-06 15:44:54 +0700

    Menambahkan eigenvector

--- src/lib/eigen/eigenval.py
+++ src/lib/eigen/eigenval.py
@@ -3,16 +3,20 @@ from numpy.linalg import qr
 from scipy.linalg import hessenberg
 
 
-def calculate_eigenvalue(M: np.ndarray):
+def calculate_eigen(M: np.ndarray):
   """Menghitung nilai eigen. M haruslah matriks persegi."""
 
   dim = M.shape[1]
   MH = hessenberg(M)
+  V = np.identity(dim)
 
-  for _ in range(dim):
+  iteration = dim//2
+
+  for _ in range(iteration):
     SHIFT = np.identity(dim) * np.diag(MH)[-1]
 
     q,r = qr(MH - SHIFT)
     MH = (r @ q) + SHIFT
+    V = V @ q
 
-  return np.sort(np.diag(MH))[::-1]
+  return np.sort(np.diag(MH))[::-1], V
